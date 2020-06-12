5782b588457df170a63c495c1624b0b26617f475
Alberto <alberto.poyatos.uxo@gmail.com>
2020-06-12 21:01:06 +0200

    Add PaymentService

---
+++ src/main/java/guru/springframework/msscssm/service/PaymentServiceImpl.java
@@ -0,0 +1,41 @@
+package guru.springframework.msscssm.service;
+
+import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import guru.springframework.msscssm.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.stereotype.Service;
+
+import static guru.springframework.msscssm.domain.PaymentState.NEW;
+
+@RequiredArgsConstructor
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+
+    @Override
+    public Payment newPayment(Payment payment) {
+        payment.setState(NEW);
+        return paymentRepository.save(payment);
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> declinePayment(Long paymentId) {
+        return null;
+    }
+}
